d7f33990598fbd6ab276d7fdb9fc7c75706b3481
Chris Eckert <chriseckert.ce@gmail.com>
2021-03-19 18:25:35 +0100

    Add Initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import { ROUTES } from './utils/routes';
+
+const NoMatch = () => <div>404</div>
 
 function App() {
   return (
-    <div className="App"></div>
+    <ChakraProvider >
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={ROUTES.SIGNUP} component={SignUp} />
+          <Route component={NoMatch} />
+        </Switch>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
